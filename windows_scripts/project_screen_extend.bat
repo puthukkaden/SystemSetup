5aad5d399452d3883c313665ca521185db823d29
DEEPAK J PUTHUKKADEN <deepakjputhukkaden@gmail.com>
2020-08-22 17:35:05 +0530

    Create project_screen_extend.bat
    Script to change the "Project Mode" to extended

---
+++ windows_scripts/project_screen_extend.bat
@@ -0,0 +1,8 @@
+@echo off
+
+rem Batch script to change "Project" mode to Extend
+rem ===== ====== == ====== ========= ==== == ======
+
+rem Change Project mode to Extend mode
+  echo Changing Project mode to Extend & echo.
+  %windir%\System32\DisplaySwitch.exe /extend
